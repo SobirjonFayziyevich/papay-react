1a057856f0447ba10904f4a376d2a2edb82fd18d
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-11-10 11:34:38 +0900

    feat: customized integration of MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import {Box, Button, Container, Stack, Typography} from '@mui/material';
 import React from 'react';
 import '../css/App.css';
+import {RippleBadge} from "./MaterialTheme/styled";
 
 function App() {
   return (
@@ -10,8 +11,13 @@ function App() {
               <Typography variant="h4" component={"h1"} gutterBottom>
                   Create React App on TypeScript with REDUX
               </Typography>
-          </Box>       {/* sx bu styleni urniga ishlatamiz, my=> margin x va y uqiga nisbatan deg.*/}
-            <Button variant="contained">Contained</Button>
+          </Box>{/* sx bu styleni urniga ishlatamiz, my=> margin x va y uqiga nisbatan deg.*/}
+            <Box>
+                <RippleBadge badgeContent={4}>
+                    <Button variant="contained">Contained</Button>
+                </RippleBadge>
+            </Box>
+
         </Stack>
       </Container>
   );
