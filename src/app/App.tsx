d0693f40a2d1c664df56a5c815800492891c9441
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-11-11 13:39:23 +0900

    feat: develop header navbarHome component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/App.css';
+import '../css/navbar.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {RestaurantPage} from "./screens/RestaurantPage";
 import {CommunityPage} from "./screens/CommunityPage";
@@ -8,41 +9,54 @@ import {MemberPage} from "./screens/MemberPage";
 import {HelpPage} from "./screens/HelpPage";
 import {LoginPage} from "./screens/LoginPage";
 import {Homepage} from "./screens/Homepage";
-function App() {
+import {NavbarHome} from "./components/header";
+import {NavbarRestaurant} from "./components/header/restaurant";
+import {NavbarOthers} from "./components/header/others";
+function App() {                                                       // useStateni REACT dan import qilib olamz.
+     const [path, setPath] = useState();  // bizning path imiz uzgarganda viewimizni qayta qurub beradi.
+    const main_path = window.location.pathname;
+    // console.log("main_path:", main_path);
+
   return (
-      <div>
-          <Router>
-              <div>
-                  {/*buyerdagilar barchasi nav link hisoblanadi.*/}
-                  <nav>
-                      <ul>
-                          <li>
-                              <Link to="restaurant">RestaurantPage</Link>
-                          </li>
-                          <li>
-                              <Link to="/community">CommunityPage</Link>
-                          </li>
-                          <li>
-                              <Link to="/orders">OrdersPage</Link>
-                          </li>
-                          <li>
-                              <Link to="/member-page">MemberPage</Link>
-                          </li>
+      <Router>
+              {main_path == "/" ? (
+                  <NavbarHome setPath={setPath}/>
+              ) : main_path.includes("/restaurant") ? (
+                  <NavbarRestaurant setPath={setPath}/>
+                  ) : (
+                      <NavbarOthers setPath={setPath}/>
+                  )}
 
-                          <li>
-                          <Link to="/help">HelpPage</Link>
-                          </li>
+                  {/*/!*buyerdagilar barchasi nav link hisoblanadi.*!/*/}
+                  {/*<nav>*/}
+                  {/*    <ul>*/}
+                  {/*        <li>*/}
+                  {/*            <Link to="restaurant">RestaurantPage</Link>*/}
+                  {/*        </li>*/}
+                  {/*        <li>*/}
+                  {/*            <Link to="/community">CommunityPage</Link>*/}
+                  {/*        </li>*/}
+                  {/*        <li>*/}
+                  {/*            <Link to="/orders">OrdersPage</Link>*/}
+                  {/*        </li>*/}
+                  {/*        <li>*/}
+                  {/*            <Link to="/member-page">MemberPage</Link>*/}
+                  {/*        </li>*/}
 
-                          <li>
-                          <Link to="/login">LoginPage</Link>
-                          </li>
+                  {/*        <li>*/}
+                  {/*        <Link to="/help">HelpPage</Link>*/}
+                  {/*        </li>*/}
 
-                          <li>
-                          <Link to="/">HomePage</Link>
-                          </li>
+                  {/*        <li>*/}
+                  {/*        <Link to="/login">LoginPage</Link>*/}
+                  {/*        </li>*/}
 
-                      </ul>
-                  </nav>
+                  {/*        <li>*/}
+                  {/*        <Link to="/">HomePage</Link>*/}
+                  {/*        </li>*/}
+
+                  {/*    </ul>*/}
+                  {/*</nav>*/}
 
                   {/*buyerdan swich routerlar boshlandi*/}
                   <Switch>
@@ -68,10 +82,8 @@ function App() {
                           < Homepage />
                       </Route>
                   </Switch>
+      </Router>
 
-              </div>
-          </Router>
-      </div>
 
   );
 }
