c0fc44b7b6e3a3714ba180e25764a518047addbc
sobirjonfayziyevich <sobirjon.0513@gmail.com>
2023-11-14 13:25:49 +0900

    feat: develop homepage statistics components

---
+++ src/app/screens/Homepage/bestDishes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Container} from "@mui/material";
+
+
+export function BestDishes() {
+    return(
+        <div className="top_restaurant_frame">
+            <Container>BestDishes</Container>
+        </div>
+    );
+}
---
+++ src/app/screens/Homepage/bestRestaurants.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Container} from "@mui/material";
+
+
+export function BestRestaurants() {
+    return(
+        <div className="top_restaurant_frame">
+            <Container>BestRestaurants</Container>
+        </div>
+    );
+}
